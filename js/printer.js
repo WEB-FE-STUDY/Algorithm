4a146b73a5f649343c11aec9971849f8efd5b612
SeungHyun <fm10033@gmail.com>
2022-02-25 15:47:55 +0900

    [PGS] 프린터 / Level 2 / 70분
    https://programmers.co.kr/learn/courses/30/lessons/42587

---
+++ js/printer.js
@@ -0,0 +1,32 @@
+function solution(priorities, location) {
+  let answer = 1;
+  let curLocation = location;
+  let target = priorities[location];
+  while (true) {
+    const max = Math.max(...priorities);
+    const check = priorities.shift();
+    if (check === max) {
+      if (curLocation === 0) break;
+      else {
+        curLocation--;
+        answer++;
+      }
+    } else {
+      priorities.push(check);
+      if (curLocation === 0) {
+        curLocation = priorities.length - 1;
+      } else {
+        curLocation--;
+      }
+      target = priorities[curLocation];
+    }
+    // console.log(
+    //   `check:${check}, curLocation:${curLocation}, priorities.length:${priorities.length}, answer: ${answer}`
+    // );
+  }
+  return answer;
+}
+
+const priorities = [1, 1, 9, 1, 1, 1];
+const location = 0;
+console.log(solution(priorities, location));

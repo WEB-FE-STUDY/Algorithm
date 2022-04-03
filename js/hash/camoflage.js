58a60de56ce0b69b3a191e1e1b1aebb14043203d
SeungHyun <fm10033@gmail.com>
2022-04-03 17:36:43 +0900

    위장 다시 풀이

--- js/hash/camoflage.js
+++ js/hash/camoflage.js
@@ -1,15 +1,19 @@
 function solution(clothes) {
-  let answer = 1;
+  let answer = 1; // 0에는 어떤 숫자를 곱해도 0이기에 1로 해줌
   const hash = {};
 
-  for (const cloth of clothes) {
-    hash[cloth[1]] = (hash[cloth[1]] || 1) + 1; //obj에 해당 키가 없으면 값을 1(옷을 입지 않은 경우)로 지정하고 1(옷의 개수)을 더해줌.
+  for (let cloth of clothes) {
+    // hash에 옷의 종류를 넣어주되, 이미 hash에 있다면 +1, 없다면 1로 선언
+    if (hash[cloth[1]]) {
+      hash[cloth[1]]++;
+    } else {
+      hash[cloth[1]] = 1;
+    }
   }
-
   for (let key in hash) {
     answer *= hash[key];
   }
-  return answer - 1;
+  return answer - 1; // 하나도 안 입엇을 경우 제거
 }
 const cloth1 = [
   ["yellowhat", "headgear"],

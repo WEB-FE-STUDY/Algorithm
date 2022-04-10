d53caf8e0f499b8a9db8b88e57872deede0c675f
SeungHyun <fm10033@gmail.com>
2022-04-10 23:52:13 +0900

    [PGS] 디스크 컨트롤러 / level 3 / 180분

--- js/hash/bestAlbum.js
+++ js/hash/bestAlbum.js
@@ -1,5 +1,37 @@
 function solution(genres, plays) {
   let answer = [];
+  const songs = [];
+  const hash = {};
+  for (let i = 0; i < genres.length; i++) {
+    songs[i] = { genre: genres[i], play: plays[i], idx: i };
+  }
+  // const sortedSongs = songs.sort((a, b) => {
+  //   const [genreA, genreB] = [a.genre, b.genre];
+  //   if (genreA < genreB) {
+  //     return -1;
+  //   }
+  //   if (genreA > genreB) {
+  //     return 1;
+  //   }
+  //   return 0;
+  // });
+  const sortedSongs = songs.sort((a, b) => {
+    return b.play - a.play;
+  });
+  console.log(sortedSongs);
+  for (let i = 0; i < sortedSongs.length; i++) {
+    let song = sortedSongs[i];
+    if (hash[song.genre]) {
+      hash[song.genre] = [...hash[song.genre], song.idx];
+    } else {
+      hash[song.genre] = [song.idx];
+    }
+  }
+  console.log(hash);
+  for (let genre of Object.values(hash)) {
+    answer.push(genre[0], genre[1]);
+  }
+
   return answer;
 }
 
--- js/hash/camoflage.js
+++ js/hash/camoflage.js
@@ -1,7 +1,6 @@
 function solution(clothes) {
   let answer = 1; // 0에는 어떤 숫자를 곱해도 0이기에 1로 해줌
   const hash = {};
-
   for (let cloth of clothes) {
     // hash에 옷의 종류를 넣어주되, 이미 hash에 있다면 +1, 없다면 1로 선언
     if (hash[cloth[1]]) {
@@ -10,8 +9,9 @@ function solution(clothes) {
       hash[cloth[1]] = 1;
     }
   }
+  console.log(hash);
   for (let key in hash) {
-    answer *= hash[key];
+    answer *= hash[key] + 1;
   }
   return answer - 1; // 하나도 안 입엇을 경우 제거
 }

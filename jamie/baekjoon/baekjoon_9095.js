e513953ad30b93691359de35398113cb4f21f056
mina-gwak <mina.gwak@kakao.com>
2022-08-01 16:39:28 +0900

    [BOJ] 1, 2, 3 더하기 / 실버 3 / 40분

---
+++ jamie/baekjoon/baekjoon_9095.js
@@ -0,0 +1,18 @@
+// 1, 2, 3 더하기
+// https://www.acmicpc.net/problem/9095
+
+const fs = require('fs');
+const [, ...inputs] = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);
+
+const counts = [];
+
+counts[1] = 1;
+counts[2] = 2;
+counts[3] = 4;
+
+for (let i = 4; i <= 10; i++) {
+  counts[i] = counts[i - 1] + counts [i - 2] + counts[i - 3];
+}
+
+console.log(inputs.map(num => counts[num]).join('\n'));
+기

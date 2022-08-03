8838ecfd5641d1ac83f4ddf008e2b1789aa7d5f8
SeungHyun <fm10033@gmail.com>
2022-08-03 14:57:59 +0900

    [BOJ] N과M (3) /silver3/5분

---
+++ js/BaekJun/15651.js
@@ -0,0 +1,34 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const solution = (input) => {
+  const [range, selectNum] = input.split(" ").map(Number);
+  const rangeArr = [];
+  for (let i = 1; i <= range; i++) {
+    rangeArr.push(i);
+  }
+  const getCombinations = (arr, selectNumber) => {
+    const results = [];
+    if (selectNumber === 1) return arr.map((el) => [el]);
+    arr.forEach((fixed) => {
+      const rest = arr;
+      const combinations = getCombinations(rest, selectNumber - 1);
+      const attatched = combinations.map((el) => [fixed, ...el]);
+      results.push(...attatched);
+    });
+    return results;
+  };
+  const answer = getCombinations(rangeArr, selectNum)
+    .map((arr) => arr.join(" ").split(","))
+    .join("\n");
+  return answer;
+};
+
+rl.on("line", (answer) => {
+  console.log(solution(answer));
+  rl.close();
+});
